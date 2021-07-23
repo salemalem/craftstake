efcfb6b71b1b27fd990cc6a9247bad93b6d6c7b9
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-07-23 15:05:01 +0600

    events that will trigger front end added. Allowing 3rd party to spend your funds feature.

--- trc20token.sol
+++ trc20token.sol
@@ -9,6 +9,11 @@ contract TRC20 {
     address public owner;
     
     mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address=> uint256)) public allowance;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 value);
+    event EcoBurn(address indexed from, uint256 value);
     
     constructor() public {
         decimals = 8;
@@ -29,13 +34,30 @@ contract TRC20 {
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
         
-        // emit Transfer(_from, _to_, _value);
+        emit Transfer(_from, _to, _value);
         
         assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
     
     function transfer(address _to, uint256 _value) public returns(bool success) {
         _transfer(msg.sender, _to, _value);
+        
+        return true;
+    }
+    
+    // 3rd party can spend someone's fund if that person approved it using approve function
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);
+        _transfer(_from, _to, _value);
+
+        return true;
+    }
+    
+    // approve 3rd party to spend your tokens
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        
         return true;
     }
 }
