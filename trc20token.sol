8f8526494c5578b0327784be038c955071ba1dc1
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-07-28 16:13:18 +0600

    Error messages are attached to require to debug easier

--- trc20token.sol
+++ trc20token.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-contract JDoge {
+contract CraftStakeToken {
 
     string public name; // token name
     string public symbol; // token symbol
@@ -8,7 +8,7 @@ contract JDoge {
     uint256 public totalSupply;
     address public owner;
     uint256 constant public maxSupply = 1000000*10**uint256(decimals);
-    
+
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address=> uint256)) public allowance;
     
@@ -23,13 +23,13 @@ contract JDoge {
         totalSupply = 1000000*10**uint256(decimals);
         owner = msg.sender;
         balanceOf[owner] = totalSupply;
-        name = "JDoge";
-        symbol = "JDG";
+        name = "CraftStake";
+        symbol = "CRS";
     }
-    
+
     function _transfer(address _from, address _to, uint256 _value) internal {
-        require(_to != address(0));
-        require(balanceOf[_from] >= _value);
+        require(_to != address(0), "Address can't be empty.");
+        require(balanceOf[_from] >= _value, "You are trying to spend more than you have.");
         require(balanceOf[_to] + _value >= balanceOf[_to]);
         
         uint previousBalances = balanceOf[_from] + balanceOf[_to];
@@ -49,7 +49,7 @@ contract JDoge {
     
     // spend 3rd party's approved token amount
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(allowance[_from][msg.sender] >= _value);
+        require(allowance[_from][msg.sender] >= _value, "You can't spend more than allowed");
         
         allowance[_from][msg.sender] -= _value;
         _transfer(_from, _to, _value);
@@ -67,7 +67,7 @@ contract JDoge {
     
     // disappear from blockchain supply forever
     function ecoburn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
+        require(balanceOf[msg.sender] >= _value, "You can't burn more than you have.");
         
         balanceOf[msg.sender] -= _value;
         totalSupply -= _value;
@@ -79,8 +79,8 @@ contract JDoge {
     
     // eco burn tokens of 3rd party
     function ecoburnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);
-        require(allowance[_from][msg.sender] >= _value);
+        require(balanceOf[_from] >= _value, "You can't burn more than you have.");
+        require(allowance[_from][msg.sender] >= _value, "You can't burn more than you are allowed.");
         
         balanceOf[_from] -= _value;
         allowance[_from][msg.sender] -= _value;
@@ -93,8 +93,8 @@ contract JDoge {
     
     // generate new tokens (only owner can do it)
     function mint(uint256 _value) public returns (bool success) {
-        require(msg.sender == owner);
-        require(totalSupply + _value <= maxSupply);
+        require(msg.sender == owner, "Only owner can mint.");
+        require(totalSupply + _value <= maxSupply, "Minted token supply can't exceed max fixed supply.");
         
         balanceOf[owner] += _value;
         totalSupply += _value;
@@ -106,8 +106,8 @@ contract JDoge {
     
     // generate new tokens to 3rd party (only owner can do it)
     function mintTo(address _to, uint256 _value) public returns (bool success) {
-        require(msg.sender == owner);
-        require(_to != address(0));
+        require(msg.sender == owner, "Only owner can mint.");
+        require(_to != address(0), "You can't mint to empty address.");
         require(totalSupply + _value <= maxSupply);
         
         balanceOf[_to] += _value;
@@ -120,9 +120,9 @@ contract JDoge {
     
     // owner assigns new owner
     function transferOwnership(address newOwner) public returns (bool success) {
-        require(newOwner != address(0));
-        require(newOwner != owner);
-        require(msg.sender == owner);
+        require(newOwner != address(0), "You can't transfer ownership to empty address.");
+        require(newOwner != owner, "New owner is the same as old owner.");
+        require(msg.sender == owner, "Only owner can transfer ownership");
         
         emit OwnershipTransferred(owner, newOwner);
         owner = newOwner;
